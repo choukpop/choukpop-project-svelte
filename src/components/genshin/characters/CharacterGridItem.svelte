dc8b888ad6d5fb23fd7e22c5e6f9ef6a21fa42c3
choukpop <choukpop1337@gmail.com>
2023-04-19 03:40:52 +0100

    add all characters and filters

---
+++ src/components/genshin/characters/CharacterGridItem.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    export let id: any;
+    export let char: any;
+</script>
+
+<a
+    href="/genshin/characters/{id}"
+    class="m-4 cell relative cursor-pointer transition duration-100 hover:opacity-100 hover:shadow-xl hover:scale-110 rounded-xl"
+>
+    <div class="w-full rounded-t-xl bg-opacity-50 overflow-hidden {char.rarity === 5 ? 'bg-legendary-from' : 'bg-rare-from'}">
+        <img
+            class="w-full h-full"
+            width="106"
+            height="106"
+            src={`/images/characters/${id}.png`}
+            alt={char.name}
+        />
+    </div>
+    <div
+        class="absolute top-0 right-0 bg-black bg-opacity-75 rounded-full flex items-center shadow-md text-white"
+        style="padding: 4px; margin: -10px;"
+    >
+        <img
+            class="w-4 h-4"
+            src={`/images/elements/${char.element.id}.png`}
+            alt={char.element.name}
+        />
+    </div>
+    <div
+        class="relative overflow-hidden rounded-b-xl"
+        style="height: 29px; background-color: var(--secondary-color);"
+    >
+        <div class="w-full overflow-hidden absolute bottom-0">
+            <p class="text-white p-1 text-center text-sm">
+                {char.name}
+            </p>
+        </div>
+    </div>
+</a>
+
+<style lang="postcss">
+    .small {
+        font-size: 12px;
+        line-height: 1;
+        padding-bottom: 3px;
+        word-spacing: 200px;
+    }
+</style>
