7c5220388b3e73361a9d65fe55323afcbc8a6846
choukpop <choukpop1337@gmail.com>
2023-04-25 00:49:06 +0100

    add display build

---
+++ src/data/displayBuild.js
@@ -0,0 +1,277 @@
+import { weaponList } from "./weaponList"
+
+
+export const displayBuild = {
+    albedo: {
+        id: 'albedo',
+        signarute: weaponList.cinnabar_spindle
+    },
+    alhaitham: {
+        id: 'alhaitham',
+        signarute: weaponList.light_of_foliar_incision
+    },
+    aloy: {
+        id: 'aloy',
+        signarute: weaponList.predator
+    },
+    amber: {
+        id: 'amber',
+        signarute: weaponList.rust
+    },
+    arataki_itto: {
+        id: 'arataki_itto',
+        signarute: weaponList.redhorn_stonethresher
+    },
+    barbara: {
+        id: 'barbara',
+        signarute: weaponList.favonius_codex
+    },
+    beidou: {
+        id: 'beidou',
+        signarute: weaponList.blackcliff_slasher
+    },
+    bennett: {
+        id: 'bennett',
+        signarute: weaponList.fillet_blade
+    },
+    candace: {
+        id: 'candace',
+        signarute: weaponList.royal_spear
+    },
+    chongyun: {
+        id: 'chongyun',
+        signarute: weaponList.skyrider_sword
+    },
+    collei: {
+        id: 'collei',
+        signarute: weaponList.kings_squire
+    },
+    cyno: {
+        id: 'cyno',
+        signarute: weaponList.staff_of_the_scarlet_sands
+    },
+    dehya: {
+        id: 'dehya',
+        signarute: weaponList.beacon_of_the_reed_sea
+    },
+    diluc: {
+        id: 'diluc',
+        signarute: weaponList.wolfs_gravestone
+    },
+    diona: {
+        id: 'diona',
+        signarute: weaponList.sacrificial_bow
+    },
+    dori: {
+        id: 'dori',
+        signarute: weaponList.makhaira_aquamarine
+    },
+    eula: {
+        id: 'eula',
+        signarute: weaponList.song_of_broken_pines
+    },
+    faruzan: {
+        id: 'faruzan',
+        signarute: weaponList.the_stringless
+    },
+    fischl: {
+        id: 'fischl',
+        signarute: weaponList.mitternachts_waltz
+    },
+    ganyu: {
+        id: 'ganyu',
+        signarute: weaponList.amos_bow
+    },
+    gorou: {
+        id: 'gorou',
+        signarute: weaponList.raven_bow
+    },
+    hu_tao: {
+        id: 'hu_tao',
+        signarute: weaponList.staff_of_homa
+    },
+    jean: {
+        id: 'jean',
+        signarute: weaponList.aquila_favonia
+    },
+    kaedehara_kazuha: {
+        id: 'kaedehara_kazuha',
+        signarute: weaponList["freedom-sworn"]
+    },
+    kaeya: {
+        id: 'kaeya',
+        signarute: weaponList.cool_steel
+    },
+    kamisato_ayaka: {
+        id: 'kamisato_ayaka',
+        signarute: weaponList.mistsplitter_reforged
+    },
+    kamisato_ayato: {
+        id: 'kamisato_ayato',
+        signarute: weaponList.haran_geppaku_futsu
+    },
+    keqing: {
+        id: 'keqing',
+        signarute: weaponList.lions_roar
+    },
+    klee: {
+        id: 'klee',
+        signarute: weaponList.dodoco_tales
+    },
+    kujou_sara: {
+        id: 'kujou_sara',
+        signarute: weaponList.hamayumi
+    },
+    kuki_shinobu: {
+        id: 'kuki_shinobu',
+        signarute: weaponList.festering_desire
+    },
+    layla: {
+        id: 'teste',
+        signarute: weaponList.xiphos_moonlight
+    },
+    lisa: {
+        id: 'lisa',
+        signarute: weaponList.thrilling_tales_of_dragon_slayers
+    },
+    mika: {
+        id: 'mika',
+        signarute: weaponList.missive_windspear
+    },
+    mona: {
+        id: 'mona',
+        signarute: weaponList.mappa_mare
+    },
+    nahida: {
+        id: 'nahida',
+        signarute: weaponList.a_thousand_floating_dreams
+    },
+    nilou: {
+        id: 'nilou',
+        signarute: weaponList["key_of_khaj-nisut"]
+    },
+    ningguang: {
+        id: 'ningguang',
+        signarute: weaponList.memory_of_dust
+    },
+    noelle: {
+        id: 'noelle',
+        signarute: weaponList.favonius_greatsword
+    },
+    qiqi: {
+        id: 'qiqi',
+        signarute: weaponList.sacrificial_sword
+    },
+    raiden_shogun: {
+        id: 'raiden_shogun',
+        signarute: weaponList.engulfing_lightning
+    },
+    razor: {
+        id: 'razor',
+        signarute: weaponList.serpent_spine
+    },
+    rosaria: {
+        id: 'rosaria',
+        signarute: weaponList.black_tassel
+    },
+    sangonomiya_kokomi: {
+        id: 'sangonomiya_kokomi',
+        signarute: weaponList.everlasting_moonglow
+    },
+    sayu: {
+        id: 'sayu',
+        signarute: weaponList.katsuragikiri_nagamasa
+    },
+    shenhe: {
+        id: 'shenhe',
+        signarute: weaponList.calamity_queller
+    },
+    shikanoin_heizou: {
+        id: 'shikanoin_heizou',
+        signarute: weaponList.hakushin_ring
+    },
+    sucrose: {
+        id: 'sucrose',
+        signarute: weaponList.lost_prayer_to_the_sacred_winds
+    },
+    tartaglia: {
+        id: 'tartaglia',
+        signarute: weaponList.polar_star
+    },
+    thoma: {
+        id: 'thoma',
+        signarute: weaponList.kitain_cross_spear
+    },
+    tighnari: {
+        id: 'tighnari',
+        signarute: weaponList.hunters_path
+    },
+    traveler_anemo: {
+        id: 'traveler_anemo',
+        signarute: weaponList.dull_blade
+    },
+    traveler_geo: {
+        id: 'traveler_geo',
+        signarute: weaponList.dull_blade
+    },
+    traveler_electro: {
+        id: 'traveler_electro',
+        signarute: weaponList.dull_blade
+    },
+    traveler_dendro: {
+        id: 'traveler_dendro',
+        signarute: weaponList.dull_blade
+    },
+    venti: {
+        id: 'venti',
+        signarute: weaponList.skyward_harp
+    },
+    wanderer: {
+        id: 'wanderer',
+        signarute: weaponList.tulaytullahs_remembrance
+    },
+    xiangling: {
+        id: 'xiangling',
+        signarute: weaponList.white_tassel
+    },
+    xiao: {
+        id: 'xiao',
+        signarute: weaponList["primordial_jade_winged-spear"]
+    },
+    xingqiu: {
+        id: 'xingqiu',
+        signarute: weaponList.royal_longsword
+    },
+    xinyan: {
+        id: 'xinyan',
+        signarute: weaponList.bloodtainted_greatsword
+    },
+    yae_miko: {
+        id: 'yae_miko',
+        signarute: weaponList.kaguras_verity
+    },
+    yanfei: {
+        id: 'yanfei',
+        signarute: weaponList.blackcliff_agate
+    },
+    yaoyao: {
+        id: 'yaoyao',
+        signarute: weaponList.lithic_spear
+    },
+    yelan: {
+        id: 'yelan',
+        signarute: weaponList.aqua_simulacra
+    },
+    yoimiya: {
+        id: 'yoimiya',
+        signarute: weaponList.thundering_pulse
+    },
+    yun_jin: {
+        id: 'yun_jin',
+        signarute: weaponList.dragons_bane
+    },
+    zhongli: {
+        id: 'zhongli',
+        signarute: weaponList.vortex_vanquisher
+    }
+}
